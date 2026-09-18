4d9e0fef20e815cf443363cffd678d80ee17ebbd
Zevor <coldstorm34@gmail.com>
2026-09-19 01:02:32 +0300

    Ignore Control Center build artifacts

--- desktop/control-center/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_zethropol-control-center_FILE /home/Zevor/Zethropol-OS/desktop/control-center/build/zethropol-control-center)
-set(__QT_DEPLOY_TARGET_zethropol-control-center_TYPE EXECUTABLE)
--- desktop/control-center/build/.qt/zethropol-control-center_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "zethropol-control-center")
-set(working_dir "/home/Zevor/Zethropol-OS/desktop/control-center")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/Zevor/Zethropol-OS/desktop/control-center/build/.qt/zethropol-control-center_res.txt")
